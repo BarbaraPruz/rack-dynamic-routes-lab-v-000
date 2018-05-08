ccf2c1a047d8851cb1cace5fab2113989ad36412
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-08 00:18:10 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+
+  @@items = []
+ 
+  def call(env)
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+          item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+          item = @@items.find{|s| s.name == item_name}
+     
+          resp.write item.price
+          end
+
+        else
+          resp.write "Path Not Found"
+          resp.status = 404
+        end
+
+        resp.finish
+  end
+end
