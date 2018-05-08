bd998c81284be2bb3f6cb0806292bc4de66df80d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-08 00:19:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
         if req.path.match(/items/)
           item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
           item = @@items.find{|s| s.name == item_name}
-          if item 
+          if item
             resp.write item.price
           else
             resp.write "Item not found"
